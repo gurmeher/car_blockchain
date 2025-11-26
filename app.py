ca0e6c2d5d8ec47ce12933241dae893876eb724c
gurmeher <gurmeherbhasin@gmail.com>
2025-11-25 21:20:47 -0800

    fix: auto start render

--- app.py
+++ app.py
@@ -155,6 +155,7 @@ def odometer_update():
     }), 201
 
 
-#---------------------- Run the app ---------------------#
+'''#---------------------- Run the app ---------------------#
 if __name__ == "__main__": #if file run, name becomes main and this will run
     app.run(host="0.0.0.0", port=3000)
+'''
